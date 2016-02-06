09e3bfa6aca47182b23f654f1920023a0afcf10d
Ivy Wong <ivy.wong022@gmail.com>
2016-02-06 17:45:09 -0500

    Initial commit

---
+++ app/src/main/java/com/hab/studyspace/StudySpace.java
@@ -0,0 +1,21 @@
+package com.hab.studyspace;
+
+//import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+
+/**
+ * Created by ivy on 2/6/16.
+ */
+//@DynamoDBTable(tableName = "study-spaces")
+public class StudySpace {
+    private String name;
+
+    //@DynamoDBIndexRangeKey(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
