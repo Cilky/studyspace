40a2cd429ccd2d0fcedd2cb845cfb30c2658d594
Ivy Wong <ivy.wong022@gmail.com>
2016-02-07 09:38:07 -0500

    Add icons

--- app/src/main/java/com/hab/studyspace/MainActivity.java
+++ app/src/main/java/com/hab/studyspace/MainActivity.java
@@ -104,13 +104,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_map) {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            MainActivity.this.startActivity(intent);
+        } else if (id == R.id.nav_fave) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_share) {
 
